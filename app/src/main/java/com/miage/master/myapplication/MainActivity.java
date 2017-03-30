1386ddca1ea683c257fa19c423c0b9b6c932bc10
Vincent  Destrieux <destrieux.vincent@gmail.com>
2017-03-30 14:02:42 +0200

    Lien entre page 1 et 2

--- app/src/main/java/com/miage/master/myapplication/MainActivity.java
+++ app/src/main/java/com/miage/master/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.miage.master.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void difficulty(View v){
-
+        Intent intent = new Intent(MainActivity.this, Niveau.class);
+        startActivity(intent);
     }
 
     public void exitApp(View v){
