facd13f0917c8c783127b4707062ca5a97650ee9
Vincent  Destrieux <destrieux.vincent@gmail.com>
2017-03-30 14:58:42 +0200

    Classe Grille (3eme ecran)

---
+++ app/src/main/java/com/miage/master/myapplication/Grille.java
@@ -0,0 +1,13 @@
+package com.miage.master.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Grille extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grille);
+    }
+}
---
+++ app/src/test/java/com/miage/master/myapplication/GrilleTest.java
@@ -0,0 +1,13 @@
+package com.miage.master.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrilleTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
