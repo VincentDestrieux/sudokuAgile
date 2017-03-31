02e636a7a54a5a3b428b596cae19888d8536f117
rgermain <r.germain5@gmail.com>
2017-03-31 11:04:29 +0200

    Trest commit branch

--- app/src/main/java/com/miage/master/myapplication/MainActivity.java
+++ app/src/main/java/com/miage/master/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-//Test
+//Test Branch
 public class MainActivity extends AppCompatActivity {
 
     @Override
