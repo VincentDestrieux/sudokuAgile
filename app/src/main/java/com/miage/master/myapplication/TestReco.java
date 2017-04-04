40eb2697c83b33958c9ee4f408cf4ea404d64d6c
Vincent  Destrieux <destrieux.vincent@gmail.com>
2017-04-04 11:26:04 +0200

    Zone de Dessin

--- app/src/main/java/com/miage/master/myapplication/TestReco.java
+++ app/src/main/java/com/miage/master/myapplication/TestReco.java
@@ -1,14 +1,81 @@
 package com.miage.master.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 
 public class TestReco extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_testreco);
+
+        setContentView(new ZoneDeDessin(this));
     }
 
+    class ZoneDeDessin extends View {
+
+        private Path drawPath;
+        private Paint drawPaint, canvasPaint;
+        private static final int paintColor = Color.BLACK;
+        private Canvas drawCanvas;
+        private Bitmap canvasBitmap;
+
+        public ZoneDeDessin(Context context) {
+            super(context);
+            drawPath = new Path();
+            drawPaint = new Paint();
+            drawPaint.setColor(paintColor);
+            drawPaint.setAntiAlias(true);
+            drawPaint.setStrokeWidth(20);
+            drawPaint.setStyle(Paint.Style.STROKE);
+            drawPaint.setStrokeJoin(Paint.Join.ROUND);
+            drawPaint.setStrokeCap(Paint.Cap.ROUND);
+            canvasPaint = new Paint(Paint.DITHER_FLAG);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            drawCanvas = new Canvas(canvasBitmap);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+            canvas.drawPath(drawPath, drawPaint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float touchX = event.getX();
+            float touchY = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    drawPath.moveTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    drawPath.lineTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    drawCanvas.drawPath(drawPath, drawPaint);
+                    drawPath.reset();
+                    break;
+                default:
+                    return false;
+            }
+            invalidate();
+            return true;
+        }
+    }
 }
