971e26c20318745f1aa45be68cffe3058b895482
Vincent  Destrieux <destrieux.vincent@gmail.com>
2017-04-05 15:05:32 +0200

    Passage a une vue xml, avec Ajout dans Java

--- app/src/main/java/com/miage/master/myapplication/TestReco.java
+++ app/src/main/java/com/miage/master/myapplication/TestReco.java
@@ -3,31 +3,44 @@ package com.miage.master.myapplication;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class TestReco extends Activity {
+    private static final int ECRAN = 900;
+
+    private RelativeLayout rl;
+    private ZoneDeDessin z;
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Ancienne version mets en plein écran
         //setContentView(new ZoneDeDessin(this));
+        setContentView(R.layout.activity_testreco);
+        b = (Button) findViewById(R.id.button);
+        addZoneDeDessin();
 
-        creerVue();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
-    private void creerVue() {
-        RelativeLayout r = new RelativeLayout(this);
-        r.setBackgroundColor(Color.parseColor("#81ffdf"));
-        r.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    //Cette méthode permet de creer la Zone de dessin et de l'inclure dans la Vue.
+    private void addZoneDeDessin() {
+        rl = (RelativeLayout) findViewById(R.id.relativeL);
 
-        ZoneDeDessin z = new ZoneDeDessin(this);
+        z = new ZoneDeDessin(this);
         z.setBackgroundColor(Color.WHITE);
-        z.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,900));
+        z.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ECRAN));
 
-        r.addView(z);
-        setContentView(r);
+        rl.addView(z);
     }
 
 }
