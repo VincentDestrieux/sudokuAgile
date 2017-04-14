ada95a99896ac9320f988dd87eb585df049639cc
RemDelm <azowned@hotmail.fr>
2017-04-14 09:04:04 +0200

    CNndroid

--- app/src/main/java/com/miage/master/myapplication/vue/MainActivity.java
+++ app/src/main/java/com/miage/master/myapplication/vue/MainActivity.java
@@ -3,15 +3,24 @@ package com.miage.master.myapplication.vue;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.RenderScript;
 import android.view.View;
 
 import com.miage.master.myapplication.R;
 
+import network.CNNdroid;
+
 public class MainActivity extends Activity {
+
+    private RenderScript myRenderScript;
+    private CNNdroid myConv = null;
+    private boolean condition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //myRenderScript = RenderScript.create(this);
+        //new prepareModel().execute(myRenderScript);
         setContentView(R.layout.activity_main);
     }
 
@@ -23,4 +32,35 @@ public class MainActivity extends Activity {
     public void exitApp(View v) {
         finish();
     }
+
+    //Async task
+    /*
+    private class prepareModel extends AsyncTask<RenderScript, Void, CNNdroid> {
+
+        ProgressDialog progDailog;
+
+        protected void onPreExecute() {
+            progDailog = new ProgressDialog(MainActivity.this);
+            progDailog.setMessage("Starting...");
+            progDailog.setIndeterminate(false);
+            progDailog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progDailog.setCancelable(true);
+            progDailog.show();
+        }
+
+        @Override
+        protected CNNdroid doInBackground(RenderScript... params) {
+            try {
+                myConv = new CNNdroid(myRenderScript, "/sdcard/cnndroid/LeNet_def.txt");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return myConv;
+        }
+
+        protected void onPostExecute(CNNdroid result) {
+            condition = true;
+            progDailog.dismiss();
+        }
+    }*/
 }
