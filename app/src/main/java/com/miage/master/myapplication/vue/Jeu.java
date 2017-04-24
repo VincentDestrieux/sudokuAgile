9e32a7b7111c7479984d70569bcc7eb3b38bcb08
hamza <hamza.elfareh@gmail.com>
2017-04-24 14:17:08 +0200

    Commentaire de la classe Génération Grille

--- app/src/main/java/com/miage/master/myapplication/vue/Jeu.java
+++ app/src/main/java/com/miage/master/myapplication/vue/Jeu.java
@@ -3,7 +3,7 @@ package com.miage.master.myapplication.vue;
 import android.content.Context;
 
 import com.miage.master.myapplication.model.GrilleDeJeu;
-import com.miage.master.myapplication.service.Generator;
+import com.miage.master.myapplication.service.GenerationGrille;
 
 /**
  * Created by Vincent  Destrieux on 13/04/2017.
@@ -15,6 +15,7 @@ public class Jeu {
     private GrilleDeJeu grid = null;
 
     private int position_X = -1, position_Y = -1;
+
 
     private Jeu() {
     }
@@ -39,23 +40,35 @@ public class Jeu {
         int[][] sudoku = null;
         switch (niveau) {
             case "1":
-                sudoku = Generator.getInstance().generateGrid(1);
+                sudoku = GenerationGrille.getInstance().genereGrille(1);
                 break;
             case "2":
-                sudoku = Generator.getInstance().generateGrid(2);
+                sudoku = GenerationGrille.getInstance().genereGrille(2);
                 break;
             case "3":
-                sudoku = Generator.getInstance().generateGrid(3);
+                sudoku = GenerationGrille.getInstance().genereGrille(3);
                 break;
             case "4":
-                sudoku = Generator.getInstance().generateGrid(4);
+                sudoku = GenerationGrille.getInstance().genereGrille(4);
                 break;
             case "5":
-                sudoku = Generator.getInstance().generateGrid(5);
+                sudoku = GenerationGrille.getInstance().genereGrille(5);
                 break;
         }
+
+     /*   int[][] sudoku = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 4, 7, 6, 9, 1, 8, 2, 0},
+                {0, 3, 0, 0, 0, 0, 0, 4, 0},
+                {0, 6, 0, 3, 0, 2, 0, 7, 0},
+                {0, 8, 0, 0, 7, 0, 0, 6, 0},
+                {0, 2, 0, 1, 0, 5, 0, 3, 0},
+                {0, 9, 0, 0, 0, 0, 0, 8, 0},
+                {0, 7, 6, 8, 1, 4, 9, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0}};*/
+
         grid = new GrilleDeJeu(context);
         grid.setGrilleDeJeu(sudoku);
+
     }
 
     public GrilleDeJeu getGrid() {
@@ -83,4 +96,6 @@ public class Jeu {
         //Ici vérifier que le jeu n'est pas terminé et donc gagné fonction a implementer
         //grid.checkGame();
     }
+
+
 }
